5d77e5e4fa95301c80ca21908eacab0c920ac999
juliomoralez <juliomoralez@yandex.ru>
2019-12-28 11:15:57 +0400

    0.1

---
+++ src/main/java/pack/config/FtlConfig.java
@@ -0,0 +1,22 @@
+package pack.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+@Configuration
+@ComponentScan("pack.*")
+public class FtlConfig {
+    @Bean(name = "viewResolver")
+    public ViewResolver getViewResolver() {
+        FreeMarkerViewResolver viewResolver = new FreeMarkerViewResolver();
+        System.out.println("Create Bean viewResolver");
+
+        viewResolver.setCache(true);
+        viewResolver.setPrefix("");
+        viewResolver.setSuffix(".ftl");
+        return viewResolver;
+    }
+}
